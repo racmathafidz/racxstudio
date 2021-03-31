b72cb5161cea5adb56b315e91f53f25ea6fd0f11
racmathafidz <rac.hafidz@gmail.com>
2021-03-31 20:56:40 +0700

    add project detail page

---
+++ src/pages/ProjectDetailPage.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+import Header from 'parts/Header'
+import PortfolioDetail from 'parts/PortfolioDetail'
+import Footer from 'parts/Footer'
+
+import Data from 'json/landingPage.json'
+
+export default class ProjectDetailPage extends Component {
+
+    componentDidMount() {
+        window.scrollTo(0, 0)
+    };
+    
+    render() {
+
+        const detailData = Data.portfolio.filter( item => {
+            return item._id === `${this.props.match.params.id}`;
+        })
+
+        return (
+            <>
+                <Header {...this.props}></Header>
+                <PortfolioDetail data={[detailData[0]]}></PortfolioDetail>
+                <Footer></Footer>
+            </>
+        )
+    }
+}
