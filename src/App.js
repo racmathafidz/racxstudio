1e5464df243bba2cf928b4e31901c00b72afbd39
racmathafidz <rac.hafidz@gmail.com>
2021-03-31 20:52:35 +0700

    edit import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import LandingPage from 'pages/LandingPage'
+import ProjectPage from 'pages/ProjectPage'
+import ProjectDetailPage from 'pages/ProjectDetailPage'
+import TeamPage from 'pages/TeamPage'
+import DiscussProjectPage from 'pages/DiscussProjectPage'
 
 import 'assets/css/styles.css';
 
@@ -8,6 +12,10 @@ function App() {
   return (
     <Router>
       <Route exact path="/" component={LandingPage}></Route>
+      <Route exact path="/project" component={ProjectPage}></Route>
+      <Route exact path="/project/:id" component={ProjectDetailPage}></Route>
+      <Route exact path="/team" component={TeamPage}></Route>
+      <Route exact path="/discuss-project" component={DiscussProjectPage}></Route>
     </Router>
   );
 }
