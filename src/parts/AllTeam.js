0bc1c996535b8a103225db54f846896b852eb466
racmathafidz <rac.hafidz@gmail.com>
2021-04-03 21:15:07 +0700

    edit horizontal gap grid

--- src/parts/AllTeam.js
+++ src/parts/AllTeam.js
@@ -5,7 +5,7 @@ import Fade from 'react-reveal/Fade';
 export default function AllTeam({ data }) {
     return (
         <section className="container mx-0 sm:mx-auto">
-            <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-y-8 gap-x-28 sm:gap-14 lg:gap-10 mx-16 justify-items-center">
+            <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-y-8 gap-x-32 sm:gap-14 lg:gap-10 mx-16 justify-items-center">
                 {
                     data.map( (item, index) => {
                         return (
